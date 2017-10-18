fb8fd00e8c0ed616f31d9e48693a5a7fd59abfc1
Mario Dimitrov <mario@quikfox.com>
2017-10-18 10:41:42 +0300

    Install SonataAdminBundle and FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,19 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+            // These are the other bundles the SonataAdminBundle relies on
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+
+            // And finally, the storage and SonataAdminBundle
+            new Sonata\AdminBundle\SonataAdminBundle(),
+
+			new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+
+			new FOS\UserBundle\FOSUserBundle(),
+//        	new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
